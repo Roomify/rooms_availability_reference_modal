bc9d1b80289848c1f748e889d5de8458febf0197
Salvo Scala <salvoscala@gmail.com>
2015-02-24 17:06:37 +0100

    Display events across days in calendar to illustrate entry and departure date

--- js/rooms_modal_booking.js
+++ js/rooms_modal_booking.js
@@ -57,6 +57,7 @@ Drupal.behaviors.rooms_modal_booking = {
         ignoreTimezone: false,
         editable: false,
         selectable: true,
+        handleWindowResize: true,
         dayNamesShort: [Drupal.t("Sun"), Drupal.t("Mon"), Drupal.t("Tue"), Drupal.t("Wed"), Drupal.t("Thu"), Drupal.t("Fri"), Drupal.t("Sat")],
         monthNames: [Drupal.t("January"), Drupal.t("February"), Drupal.t("March"), Drupal.t("April"), Drupal.t("May"), Drupal.t("June"), Drupal.t("July"), Drupal.t("August"), Drupal.t("September"), Drupal.t("October"), Drupal.t("November"), Drupal.t("December")],
         firstDay: firstDay,
@@ -66,6 +67,9 @@ Drupal.behaviors.rooms_modal_booking = {
           left: 'title',
           center: '',
           right: ''
+        },
+        windowResize: function(view) {
+          $(value[0]).fullCalendar('refetchEvents');
         },
         events: Drupal.settings.basePath + '?q=rooms/units/unit/' + Drupal.settings.roomsAvailability.roomID + '/availability/json/' + value[2] + '/' + phpmonth,
 
@@ -93,6 +97,51 @@ Drupal.behaviors.rooms_modal_booking = {
         //Remove Time from events.
         eventRender: function(event, el) {
           el.find('.fc-time').remove();
+
+          // Add a class if the event start it is not "AV" or "N/A".
+          if (el.hasClass('fc-start') && this.id != 1 && this.id != 0) {
+            el.append('<div class="event-start"/>');
+            el.find('.event-start').css('border-top-color', this.color);
+          }
+
+          // Add a class if the event end and it is not "AV" or "N/A".
+          if (el.hasClass('fc-end') && this.id != 1 && this.id != 0) {
+            el.append('<div class="event-end"/>');
+            el.find('.event-end').css('border-top-color', this.color);
+          }
+        },
+        eventAfterRender: function( event, element, view ) { 
+          // Event width.
+          var width = element.parent().width()
+          // Event colspan number.
+          var colspan = element.parent().get(0).colSpan;
+          // Single cell width.
+          var cell_width = width/colspan;
+          var half_cell_width = cell_width/2;
+
+          // Move events between table margins.
+          element.css('margin-left', half_cell_width);
+          element.css('margin-right', half_cell_width);
+
+          // Calculate width event to add end date triangle.
+          width_event = element.children('.fc-content').width();
+
+          // Add a margin left to the top triangle.
+          element.children().closest('.event-end').css('margin-left', width_event-23);
+
+          // If the event end in a next row.
+          if(element.hasClass('fc-not-end')) {
+            element.css('margin-right', 0);
+          }
+          // If the event start in a previous row.
+          if(element.hasClass('fc-not-start')) {
+            // Fixes to work well with jquery 1.7.
+            if (colspan == 1) {
+              width_event = 0;
+            }
+            element.css('margin-left', 0);
+            element.children().closest('.event-end').css('margin-left', width_event);
+          }
         }
       });
     });
